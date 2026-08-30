9e00f44244288879f6c48e7eb8b65c3f860824bd
Ryosuke Miyazaki <notzibli@outlook.com>
2026-08-31 01:20:35 +0900

    Wire content script: adapter + measurement engine + 5s flush

---
+++ lib/hash.ts
@@ -0,0 +1,8 @@
+export function hashItemId(id: string): string {
+  let hash = 2166136261;
+  for (let i = 0; i < id.length; i++) {
+    hash ^= id.charCodeAt(i);
+    hash = Math.imul(hash, 16777619);
+  }
+  return (hash >>> 0).toString(16);
+}
